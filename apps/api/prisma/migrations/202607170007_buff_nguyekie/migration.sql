4b1c21a37ef6e358631b592d28ab76953859b1f6
nguyekie <kien7bnt@gmail.com>
2026-07-17 15:51:57 +0700

    Improve combat stats effects and buff nguyekie

---
+++ apps/api/prisma/migrations/202607170007_buff_nguyekie/migration.sql
@@ -0,0 +1,6 @@
+INSERT INTO "UserCurrency" ("userId", "code", "balance")
+SELECT "userId", 'GOLD'::"CurrencyCode", 1000000
+FROM "UserProfile"
+WHERE LOWER("displayName") = 'nguyekie'
+ON CONFLICT ("userId", "code") DO UPDATE
+SET "balance" = GREATEST("UserCurrency"."balance", 1000000);
